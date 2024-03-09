04acdbf1db6f9d09ce933ba89f582b414de7ca52
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2024-03-09 14:59:28 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue,),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello, world!'),
+        ),
+        body: Center(
+          child: Text('Hello, world!'),
+        ),
+      ),
+    );
+  }
+}
+
+void main()
+{
+  runApp(MyApp());
+}
